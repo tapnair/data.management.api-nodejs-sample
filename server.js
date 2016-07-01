812c708af22dbeb047e795bbf38c00609c6c55f1
tapnair <patrick.rainsberry@autodesk.com>
2016-07-01 11:04:44 -0700

    added dropbox routes

--- server.js
+++ server.js
@@ -31,12 +31,14 @@ app.use(session({
 
 // prepare server routing
 var api = require('./routes/api');
+var dropbox = require('./routes/dropbox');
 app.use('/', express.static(__dirname + '/www')); // redirect static calls
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect static calls
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect static calls
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect static calls
 app.use('/fonts', express.static(__dirname + '/node_modules/bootstrap/dist/fonts')); // redirect static calls
 app.use('/api', api); // redirect API calls
+app.use('/dropbox', dropbox); // redirect API calls
 app.set('port', process.env.PORT || 3000); // main port
 
 // start server
